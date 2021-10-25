72b96b964937d5ddbf760f7573c52db95465a5c8
navidh86 <1605005@ugrad.cse.buet.ac.bd>
2021-10-25 21:46:33 +0600

    Implement getBootMatrix function to get bootstrap matrix of desired shape

---
+++ apples/support/Bootstrapping.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def getBootMatrix(sample_count, sequence_length):
+    # create bootstrap matrix
+    mat = np.random.choice(sequence_length, (sample_count, sequence_length), replace=True)
+
+    # count of each item per row
+    boot = np.zeros((sample_count+1, sequence_length))
+    
+    # first row is all ones
+    boot[0] = np.ones(sequence_length)
+
+    for i in range(1, sample_count+1):
+        boot[i] = np.bincount(mat[i-1], minlength=sequence_length)
+
+    return boot
