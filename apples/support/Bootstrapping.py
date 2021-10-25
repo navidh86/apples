ba0450bde63778a346d0faa3f1665257eb9e009b
navidh86 <1605005@ugrad.cse.buet.ac.bd>
2021-10-25 22:44:05 +0600

    Add a 3d version of the same boot matrix

--- apples/support/Bootstrapping.py
+++ apples/support/Bootstrapping.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 class Bootstrapping:
-    boot = []
+    boot = [] # the 2d matrix
+    boot2 = [] # 3d version of the same matrix
+    sample_count = 0
 
     @classmethod
     def getBootMatrix(cls, sample_count, sequence_length):
@@ -10,11 +12,18 @@ class Bootstrapping:
 
         # count of each item per row
         Bootstrapping.boot = np.zeros((sample_count+1, sequence_length))
+        Bootstrapping.boot2 = np.zeros((sample_count+1, 1, sequence_length))
         
         # first row is all ones
         Bootstrapping.boot[0] = np.ones(sequence_length)
+        Bootstrapping.boot2[0] = np.ones((1, sequence_length))
 
         for i in range(1, sample_count+1):
             Bootstrapping.boot[i] = np.bincount(mat[i-1], minlength=sequence_length)
+            Bootstrapping.boot2[i] = Bootstrapping.boot[i].reshape((1, sequence_length))
+
+        Bootstrapping.sample_count = sample_count
+
+        print("Shape = " + str(Bootstrapping.boot2.shape))
 
         return Bootstrapping.boot
