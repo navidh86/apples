af5b956290560747c4a0f789fcdb693e65b29698
navidh86 <1605005@ugrad.cse.buet.ac.bd>
2022-02-17 12:38:22 +0600

    keep track of placement counts as well

--- apples/support/find_support.py
+++ apples/support/find_support.py
@@ -1,24 +1,30 @@
 from apples.support.Bootstrapping import Bootstrapping
-
+import json
 
 def get_support(results):
     support = []
+    count = {}
     for result in results:
         query = result[0]["placements"][0]["n"][0]
-        count = {}
-        count['total'] = 0
+        count[query] = {}
+        count[query]['total'] = 0
         for i in range(1, Bootstrapping.sample_count+1):
             branch = result[i]["placements"][0]["p"][0][0]
-            if branch not in count:
-                count[branch] = 0
+            if branch not in count[query]:
+                count[query][branch] = 0
 
-            count[branch] += 1
-            count['total'] += 1
+            count[query][branch] += 1
+            count[query]['total'] += 1
 
         original_branch = result[0]["placements"][0]["p"][0][0]
-        if original_branch not in count:
+        if original_branch not in count[query]:
             support.append(0)
         else:
-            support.append(count[original_branch]/count['total'])
+            support.append(count[query][original_branch]/count[query]['total'])
+
+    # fp = open("placement_count.txt", "w")
+    # output = json.dumps(count)
+    # fp.write(output)
+    # fp.close()
 
     return support
