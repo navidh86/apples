d24bcfef69e96e4f6775a0c4f776649f34330595
navidh86 <1605005@ugrad.cse.buet.ac.bd>
2022-04-30 15:08:39 +0600

    Utilize minimum_alignment_overlap option

--- apples/PoolQueryWorker.py
+++ apples/PoolQueryWorker.py
@@ -105,7 +105,8 @@ class PoolQueryWorker:
         jplace = dict()
         
         start_dist = time.time()
-        obs_dist = cls.reference.get_obs_dist_support(query_seq, query_name)
+        obs_dist = cls.reference.get_obs_dist_support(query_seq, query_name, cls.options.minimum_alignment_overlap,
+                                                        cls.options.protein_seqs)
         end_dist = time.time() - start_dist
 
         for j, od in enumerate(obs_dist):
