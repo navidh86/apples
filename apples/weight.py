7c959a4757177435a66d780f87a3119dcf5060e5
navidh86 <1605005@ugrad.cse.buet.ac.bd>
2022-09-09 22:40:51 +0600

    Implement multiprocessing for weight calculation

--- apples/weight.py
+++ apples/weight.py
@@ -1,6 +1,8 @@
 import treeswift as ts
 from math import exp
 import numpy as np
+import multiprocessing as mp
+from sys import platform as _platform
 
 def jc_inverse(d):
     return 0.75 * (1 - exp(-4.0*d/3))
@@ -34,8 +36,46 @@ def jc_inverse(d):
 
 #     return w
 
+def set_refs(reference, t_dist):
+    for i, ref in enumerate(reference.refs):
+        yield (i, ref, t_dist[ref], reference.refs,)
 
 def get_weights(reference, tree, options):
+    print("Calculating weights ...")
+
+    # get the dist matrix from tree
+    t_dist = tree.distance_matrix(True)
+    refs = reference.refs
+    L = len(list(refs.items())[0][1])
+
+    w = np.zeros(L)
+    denom = np.ones(L)
+    num = np.zeros(L)
+
+    if _platform == "win32" or _platform == "win64" or _platform == "msys":
+        # if windows, multithreading is not supported until either
+        # processes can be forked in windows or apples works with spawn.
+        results_combined = list(map(lambda a: calc(a[0], *a[1:]), set_refs(reference, t_dist)))   # a.k.a starmap
+    else:
+        pool = mp.Pool(options.num_thread)
+        combined = pool.starmap(calc, set_refs(reference, t_dist))
+    
+    for c in combined:
+        num += c[0]
+        denom += c[1]
+
+    w = num / denom
+    w /= np.sum(w)
+    
+    print("Weights calculated.")
+
+    if options.weight_output_fp:
+        with open(options.weight_output_fp, "w+") as fp:
+            fp.write(" ".join((str(x) for x in w)))
+
+    return w 
+
+def get_weights_old(reference, tree, options):
     print("Calculating weights ...")
 
     # get the dist matrix from tree
@@ -67,4 +107,22 @@ def get_weights(reference, tree, options):
         with open(options.weight_output_fp, "w+") as fp:
             fp.write(" ".join((str(x) for x in w)))
 
-    return w    
+    return w 
+
+
+def calc(i, ref, t_dist, refs):
+    a2 = refs[ref]
+    L = len(a2)
+    denom = np.zeros(L)
+    num = np.zeros(L)
+    for j, ref2 in enumerate(refs):
+        if j > i:
+            b2 = refs[ref2]
+            nondash = np.logical_and(a2 != b'-', b2 != b'-')
+            hh = np.logical_and(a2 != b2, nondash)
+            tt = jc_inverse(t_dist[ref2])
+            num += tt * hh
+            denom += hh
+
+    return num, denom
+    
